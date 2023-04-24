aa5a00d889ff772b02c1ced38398fe949517145b
aye1306 <chetttha13062543@gmail.com>
2023-04-24 19:06:52 +0700

    first commit

---
+++ views/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once "components/header.php" ?>
+    <title>view index</title>
+</head>
+
+<body>
+
+
+    <?php include_once "components/scripts.php" ?>
+</body>
+
+</html>
